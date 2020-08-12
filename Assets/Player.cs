75a0263e16ec9d9a99b03499480c8a042a565769
GanzaMos <moskalik.ganza@gmail.com>
2020-08-12 14:21:50 +0300

    2 moving with screenborder clamping and padding

---
+++ Assets/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+    
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] private float padding = 0.25f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    void Start()
+    {
+        SetUpBoundaries();
+    }
+
+    private void SetUpBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal"); 
+        var newXPos = Mathf.Clamp(transform.position.x + (deltaX * moveSpeed * Time.deltaTime), xMin, xMax);
+        
+        var deltaY = Input.GetAxis("Vertical"); 
+        var newYPos = Mathf.Clamp(transform.position.y + (deltaY * moveSpeed * Time.deltaTime), yMin, yMax);
+
+        transform.position = new Vector2(newXPos , newYPos);
+    }
+}
